970270f5af079cacb11c034580683c8fdb1f1525
yoshimoriryu <kocakwk@gmail.com>
2019-02-05 21:11:33 +0700

    delete file

--- mainGUI.py
+++ mainGUI.py
@@ -3,7 +3,6 @@ import time
 import random
  
 pygame.init()
-
  
 display_width = 800
 display_height = 600
@@ -11,32 +10,25 @@ display_height = 600
 black = (0,0,0)
 white = (255,255,255)
 grey = (125,125,125)
-
 red = (200,0,0)
 green = (0,200,0)
-
 bright_red = (255,0,0)
 bright_green = (0,255,0)
- 
 block_color = (53,115,255)
- 
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('24 Game Solver')
 clock = pygame.time.Clock()
  
 gameIcon = pygame.image.load('./PNG/red_back.png')
-
 pygame.display.set_icon(gameIcon)
 
-pause = False
-#crash = True
 def shuffle():
     gameDisplay.fill(white)
-    button(str(card[0]),150+17.5,200,100,50,green,bright_green,None)
-    button(str(card[1]),275+17.5,200,100,50,green,bright_green,None)
-    button(str(card[2]),400+17.5,200,100,50,green,bright_green,None)
-    button(str(card[3]),525+17.5,200,100,50,green,bright_green,None)
+    button(str(card[0]),150+17.5,200,100,50,white,bright_green,None)
+    button(str(card[1]),275+17.5,200,100,50,white,bright_green,None)
+    button(str(card[2]),400+17.5,200,100,50,white,bright_green,None)
+    button(str(card[3]),525+17.5,200,100,50,white,bright_green,None)
     Ccard = pygame.image.load('./PNG/' + str(card[0]) + '.png')
     Dcard = pygame.image.load('./PNG/' + str(13 + card[1]) + '.png')
     Hcard = pygame.image.load('./PNG/' + str(26 + card[2]) + '.png')
@@ -53,8 +45,7 @@ def shuffle():
 #♠ Spades 40 - 52
     global isShuffled
     isShuffled = True
-    # button("Play Again",150,450,100,50,green,bright_green,game_loop)
-
+    
 def lala(x):
     return (abs(24-x))
 
@@ -100,51 +91,13 @@ def solve():
 	button('result :' + str(cur), display_width/2-50, 400,100,50,white,white,None)
 	button('operator\'s point:' + str(vall), display_width/2-100,450,200,50,white,white,None)
 	card = [random.randrange(1,13),random.randrange(1,13),random.randrange(1,13),random.randrange(1,13)]
-# button("Play Again",150,450,100,50,green,bright_green,game_loop)
 
 
-def things_dodged(count):
-    font = pygame.font.SysFont("comicsansms", 25)
-    text = font.render("Dodged: "+str(count), True, black)
-    gameDisplay.blit(text,(0,0))
- 
-def things(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
- 
-def car(x,y):
-    gameDisplay.blit(carImg,(x,y))
- 
+
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
  
- 
-def crash():
-    ####################################
-    pygame.mixer.Sound.play(crash_sound)
-    pygame.mixer.music.stop()
-    ####################################
-    largeText = pygame.font.SysFont("comicsansms",115)
-    TextSurf, TextRect = text_objects("You Crashed", largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-    
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-                
-        
-
-        button("Play Again",150,450,100,50,green,bright_green,game_loop)
-        
-        button("Quit",550,450,100,50,red,bright_red,quitgame)
-
-        pygame.display.update()
-        clock.tick(15) 
-
 def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
@@ -165,34 +118,6 @@ def quitgame():
     pygame.quit()
     quit()
 
-def unpause():
-    global pause
-    pygame.mixer.music.unpause()
-    pause = False
-    
-
-def paused():
-    ############
-    pygame.mixer.music.pause()
-    #############
-    largeText = pygame.font.SysFont("comicsansms",115)
-    TextSurf, TextRect = text_objects("Paused", largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-    
-
-    while pause:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-
-        button("Continue",150,450,100,50,green,bright_green,unpause)
-        button("Quit",550,450,100,50,red,bright_red,quitgame)
-
-        pygame.display.update()
-        clock.tick(15)   
 
 
 def game_intro():
@@ -218,36 +143,15 @@ def game_intro():
 
         pygame.display.update()
         clock.tick(15)
-        
-        
-    
-    
+         
+def rfileext():
+	pass
 
-    
+
 def game_loop():
-    global pause
     global card
     
     card = [random.randrange(1,13),random.randrange(1,13),random.randrange(1,13),random.randrange(1,13)]
-    ############
-    #pygame.mixer.music.load('jazz.wav')
-    #pygame.mixer.music.play(-1)
-    ############
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
- 
-    x_change = 0
- 
-    thing_startx = random.randrange(0, display_width)
-    thing_starty = -600
-    thing_speed = 4
-    thing_width = 100
-    thing_height = 100
- 
-    thingCount = 1
- 
-    dodged = 0
- 
     gameExit = False
     gameDisplay.fill(white)
     while not gameExit:
@@ -259,7 +163,13 @@ def game_loop():
                 quit()
             button("Shuffle",150,450,100,50,green,bright_green,shuffle)
             button("Solve",550,450,100,50,red,bright_red,solve)
-        
+            button("Input File",350,525,100,50,green,bright_green,rfileext)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    shuffle()
+                if event.key == pygame.K_RIGHT:
+                    solve()
+                
         
         pygame.display.update()
         clock.tick(60)
