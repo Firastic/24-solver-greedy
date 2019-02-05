ff71faa1bdb0174e09db0b32396ce886e6618a7b
Firastic <adyaksa.wisanggeni@gmail.com>
2019-02-05 15:31:09 +0700

    Change input message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from solve import solve
 
 if __name__ == '__main__':
 	a = int(input("Enter first number\n"))
-	b = int(input("Enter first number\n"))
-	c = int(input("Enter first number\n"))
-	d = int(input("Enter first number\n"))
+	b = int(input("Enter second number\n"))
+	c = int(input("Enter third number\n"))
+	d = int(input("Enter fourth number\n"))
 	solve([a,b,c,d])
